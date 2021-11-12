8a8641816a93e211e1699b784dc15f7892ee70de
JK-NguyenVanDung <77788428+JK-NguyenVanDung@users.noreply.github.com>
2021-11-12 18:00:01 +0700

    add bottom profile

--- app/src/main/java/com/project/kicksdrop/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/project/kicksdrop/ui/profile/ProfileFragment.java
@@ -1,33 +1,34 @@
-package com.project.kicksdrop.ui.wishlist;
+package com.project.kicksdrop.ui.profile;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.project.kicksdrop.databinding.FragmentWishlistBinding;
-import com.project.kicksdrop.databinding.FragmentWishlistBinding;
+import com.project.kicksdrop.databinding.FragmentProfileBinding;
 
-public class WishListFragment extends Fragment {
-    private WishListViewModel wishListViewModel;
-private FragmentWishlistBinding binding;
+public class ProfileFragment extends Fragment {
+
+    private ProfileViewModel profileViewModel;
+    private FragmentProfileBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
-        wishListViewModel =
-                new ViewModelProvider(this).get(WishListViewModel.class);
+        profileViewModel =
+                new ViewModelProvider(this).get(ProfileViewModel.class);
 
-    binding = FragmentWishlistBinding.inflate(inflater, container, false);
+    binding = FragmentProfileBinding.inflate(inflater, container, false);
     View root = binding.getRoot();
 
-        final TextView textView = binding.textWishlish;
-        wishListViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        final TextView textView = binding.textProfile;
+        profileViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
