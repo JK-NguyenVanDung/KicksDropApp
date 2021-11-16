74be6abb7ceb317969a1b9b7e2d5604f0b376a7a
Nguyen_user <nguyen60c@gmail.com>
2021-11-16 17:52:03 +0700

    update so many things so i dont no exactly

---
+++ app/src/main/java/com/project/kicksdrop/ui/home/HomeChild/sectionProductBranchOne.java
@@ -0,0 +1,17 @@
+package com.project.kicksdrop.ui.home.HomeChild;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.project.kicksdrop.R;
+
+public class sectionProductBranchOne extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nike);
+    }
+}
