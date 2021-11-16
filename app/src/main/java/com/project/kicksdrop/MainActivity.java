3d55b4834771e70a3a886ac38308266003e51265
197PM09434 - Trần Thành Đạt - K25T-PM <dat.197pm09434@vanlanguni.vn>
2021-11-16 11:10:04 +0700

    create method loadProduct, addCart, addProductCart,delProductCart, loadImage

--- app/src/main/java/com/project/kicksdrop/MainActivity.java
+++ app/src/main/java/com/project/kicksdrop/MainActivity.java
@@ -1,25 +1,46 @@
 package com.project.kicksdrop;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.project.kicksdrop.databinding.ActivityMainBinding;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
-private ActivityMainBinding binding;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-     binding = ActivityMainBinding.inflate(getLayoutInflater());
-     setContentView(binding.getRoot());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -31,6 +52,106 @@ private ActivityMainBinding binding;
                 .findFragmentById(R.id.nav_host_fragment_activity_main);
         NavController navCo = navHostFragment.getNavController();
         NavigationUI.setupWithNavController(binding.navView, navCo);
+
+
+        loadProduct("PD1");
+
+
+        //addProductCart("AC3","PD1",5,"#333",42);
+        //addProductCart("AC3","PD2",5,"#333",42);
+        delProductCart("AC3","PD1");
+
+
+
+
+
     }
+
+    private void loadProduct(String id){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("product/"+id);
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                for (DataSnapshot item : dataSnapshot.getChildren()){
+                    item.toString();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("Load Product", "Failed to read value.", error.toException());
+            }
+        });
+    }
+
+
+    private void addCart(String idUser){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("cart");
+
+        myRef.child(idUser).child("coupon_id").setValue("");
+        myRef.child(idUser).child("product").setValue("");
+
+
+    }
+
+
+    private void addProductCart(String idUser,String idProduct,int amount, String color,int size){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("cart");
+
+
+        myRef.child(idUser).child("product").child(idProduct).child("amount").setValue(amount);
+        myRef.child(idUser).child("product").child(idProduct).child("color").setValue(color);
+        myRef.child(idUser).child("product").child(idProduct).child("size").setValue(size);
+
+
+    }
+
+    private void delProductCart(String idUser,String idProduct){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("cart");
+
+
+        myRef.child(idUser).child("product").child(idProduct).removeValue();
+
+
+    }
+
+    private void editProductCart(String idUser,String idProduct){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("cart");
+
+
+        myRef.child(idUser).child("product").child(idProduct).removeValue();
+
+
+    }
+
+    private void loadImage(ImageView image, String imageName){
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference(imageName);
+        try {
+            File file = File.createTempFile("tmp",".jpg");
+            storageReference.getFile(file).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                    Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+                    image.setImageBitmap(bitmap);
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
 
 }
