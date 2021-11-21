22262d9c79cf759bf79180222c178bf38a6eea04
Nguyen_user <nguyen60c@gmail.com>
2021-11-21 18:22:54 +0700

    feat: add code matching product cart

--- app/src/main/java/com/project/kicksdrop/ui/wishlist/WishlistFragment.java
+++ app/src/main/java/com/project/kicksdrop/ui/wishlist/WishlistFragment.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -15,16 +19,22 @@ import com.project.kicksdrop.databinding.FragmentWishlistBinding;
 
 public class WishlistFragment extends Fragment {
 
+    TextView wishListTotalItems, wishListProductName,
+            wishListProductCost, wishListProductType;
+    Spinner wishListDropDownSize;
+    Button wishListMoreOption;
+    ImageButton wishListYellow, wishListRemoveProduct;
+
     private WishlistViewModel wishlistViewModel;
     private FragmentWishlistBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
+                             ViewGroup container, Bundle savedInstanceState) {
         wishlistViewModel =
                 new ViewModelProvider(this).get(WishlistViewModel.class);
 
-    binding = FragmentWishlistBinding.inflate(inflater, container, false);
-    View root = binding.getRoot();
+        binding = FragmentWishlistBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
 
         final TextView textView = binding.textWishlist;
         wishlistViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
@@ -36,7 +46,19 @@ public class WishlistFragment extends Fragment {
         return root;
     }
 
-@Override
+
+//    public void matching() {
+//        wishListTotalItems = (TextView) findViewById(R.id.wishlist_tv_items);
+//        wishListProductName = (TextView) findViewById(R.id.wishlist_tv_productName);
+//        wishListProductCost = (TextView) findViewById(R.id.wishlist_tv_productCost);
+//        wishListProductType = (TextView) findViewById(R.id.wishlist_tv_productType);
+//        wishListDropDownSize = (Spinner) findViewById(R.id.wishlist_spinner_dropDownSize);
+//        wishListMoreOption = (Button) findViewById(R.id.wishList_btn_moreOption);
+//        wishListYellow = (ImageButton) findViewById(R.id.wishlist_ibtn_yellow);
+//        wishListRemoveProduct = (ImageButton) findViewById(R.id.wishlist_ibtn_remove);
+//    }
+
+    @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
