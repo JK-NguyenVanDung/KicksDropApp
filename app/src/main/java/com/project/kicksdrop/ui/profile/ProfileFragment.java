425f8598d5a1d456baf4e1123bcd6e63ca43e8a4
Administrator <hoangvu.nhv.xyz@gmail.com>
2021-11-24 10:49:27 +0700

    fix conflic

--- app/src/main/java/com/project/kicksdrop/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/project/kicksdrop/ui/profile/ProfileFragment.java
@@ -43,7 +43,7 @@ public class ProfileFragment extends Fragment {
     }
 
     private void logoutUI() {
-        logout = binding.btnProfileUserSignOut;
+        logout = binding.profileBtnSignOut;
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
