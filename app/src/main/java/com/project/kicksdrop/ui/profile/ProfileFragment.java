67b8fa5315caa729558d80f8a091624e754d1ed9
Administrator <hoangvu.nhv.xyz@gmail.com>
2021-11-24 10:44:20 +0700

    chore: logout

--- app/src/main/java/com/project/kicksdrop/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/project/kicksdrop/ui/profile/ProfileFragment.java
@@ -1,9 +1,11 @@
 package com.project.kicksdrop.ui.profile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,20 +14,23 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.material.button.MaterialButtonToggleGroup;
+import com.project.kicksdrop.R;
 import com.project.kicksdrop.databinding.FragmentProfileBinding;
+import com.project.kicksdrop.ui.auth.LoginActivity;
 
 public class ProfileFragment extends Fragment {
-
     private ProfileViewModel profileViewModel;
     private FragmentProfileBinding binding;
+    private Button logout;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
+                             ViewGroup container, Bundle savedInstanceState) {
         profileViewModel =
                 new ViewModelProvider(this).get(ProfileViewModel.class);
 
-    binding = FragmentProfileBinding.inflate(inflater, container, false);
-    View root = binding.getRoot();
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
         final TextView textView = binding.textProfile;
         profileViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
@@ -33,10 +38,24 @@ public class ProfileFragment extends Fragment {
                 textView.setText(s);
             }
         });
+        logoutUI();
         return root;
     }
 
-@Override
+    private void logoutUI() {
+        logout = binding.btnProfileUserSignOut;
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Ilogout = new Intent(getContext(), LoginActivity.class);
+                startActivity(Ilogout);
+
+            }
+        });
+    }
+
+
+    @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
